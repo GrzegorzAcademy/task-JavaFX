46180025b327c5ee5f16249741e5a352e3aa99b8
Grzegorz Nagórski <sparki_21@tlen.pl>
2020-08-16 16:35:58 +0200

    Task 2

--- src/main/java/pl/javaStart/fxAction/controller/MainPainController.java
+++ src/main/java/pl/javaStart/fxAction/controller/MainPainController.java
@@ -15,35 +15,15 @@ public class MainPainController {
     private Button lowerCaseButton;
 
     public void initialize() {
-//        lowerCaseButton.setOnAction(actionEvent -> {
-//            System.out.println(actionEvent.getEventType());
-//            String orginalText = mainTextArea.getText();
-//            String newText = orginalText.toLowerCase();
-//            mainTextArea.setText(newText);
-//        });
 
-        lowerCaseButton.addEventHandler(ActionEvent.ACTION, new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                System.out.println(actionEvent.getEventType());
-                System.out.println("Wcisnięto przycisk 2");
-                String orginalText = mainTextArea.getText();
-                String newText = orginalText.toLowerCase();
-                mainTextArea.setText(newText);
-            }
-        });
-        lowerCaseButton.addEventFilter(ActionEvent.ACTION, new EventHandler<ActionEvent>() {
-            @Override
-           // addEventFilter wywołuje się wcześniej niż addEventHandler
-            public void handle(ActionEvent actionEvent) {
-                System.out.println(actionEvent.getEventType());
-                System.out.println("Wcisnieto przycisk 1");
-                String orginalText = mainTextArea.getText();
-                String newText = orginalText.toLowerCase();
-                mainTextArea.setText(newText);
+    }
 
-            }
-        });
+    public void toLowerCaseAction(ActionEvent actionEvent) {
+        System.out.println(actionEvent.getEventType());
+        System.out.println("Wcisnieto przycisk 1");
+        String orginalText = mainTextArea.getText();
+        String newText = orginalText.toLowerCase();
+        mainTextArea.setText(newText);
     }
 
 }
