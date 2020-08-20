da9b3f7025b24f0f3f02377b2cca77bc2a35d0b6
Grzegorz Nagórski <sparki_21@tlen.pl>
2020-08-20 13:07:56 +0200

    first side

--- src/main/java/pl/javaStart/fxAction/controller/MainPainController.java
+++ src/main/java/pl/javaStart/fxAction/controller/MainPainController.java
@@ -5,6 +5,8 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 public class MainPainController {
 
@@ -15,15 +17,34 @@ public class MainPainController {
     private Button lowerCaseButton;
 
     public void initialize() {
+//        mainTextArea.setOnKeyPressed(keyEvent -> System.out.println("Wciśnieto przycisk :" + keyEvent.getCharacter()));
+//        mainTextArea.setOnKeyReleased(keyEvent -> System.out.println("Zwolniono przycisk : " + keyEvent.getCharacter()));
+//        mainTextArea.setOnKeyTyped(keyEvent -> System.out.println("Wcisnięto i zwolniono przycisk :" + keyEvent.getCharacter()));
 
+
+//        mainTextArea.addEventFilter(KeyEvent.KEY_TYPED, keyEvent ->
+//                System.out.println("Wcisnięto i zwolniono przycisk :" + keyEvent.getCharacter()));
+//        mainTextArea.addEventFilter(MouseEvent.MOUSE_CLICKED,
+//                mouseEvent -> System.out.println("klick mouse" + mouseEvent.getClickCount() + " "
+//                        + mouseEvent.getButton() + " "
+//                        + mouseEvent.getButton()));
+//        mainTextArea.addEventFilter(MouseEvent.MOUSE_PRESSED,
+//                mouseEvent -> System.out.println("mouse pressed"));
+//        mainTextArea.addEventFilter(MouseEvent.MOUSE_RELEASED,
+//                mouseEvent -> System.out.println("mouse released"));
+
+        mainTextArea.addEventFilter(MouseEvent.MOUSE_ENTERED,
+                mouseEvent -> System.out.println("Mysz w obszarze area" + " współrzędna wejścia X " +
+                        mouseEvent.getX() + " współrzędna wejścia Y " +
+                        mouseEvent.getY()));
+        mainTextArea.addEventFilter(MouseEvent.MOUSE_EXITED,
+                mouseEvent -> System.out.println("Mysz poza Area"));
+//        mainTextArea.addEventFilter(MouseEvent.MOUSE_MOVED,
+//                mouseEvent -> System.out.println("mysz porusza sie w Area"));
+        mainTextArea.addEventFilter(MouseEvent.MOUSE_DRAGGED,
+                mouseEvent -> System.out.println("Mysz porusza się w Area z wciśniętym przyciskiem"));
     }
 
-    public void toLowerCaseAction(ActionEvent actionEvent) {
-        System.out.println(actionEvent.getEventType());
-        System.out.println("Wcisnieto przycisk 1");
-        String orginalText = mainTextArea.getText();
-        String newText = orginalText.toLowerCase();
-        mainTextArea.setText(newText);
-    }
 
 }
+
