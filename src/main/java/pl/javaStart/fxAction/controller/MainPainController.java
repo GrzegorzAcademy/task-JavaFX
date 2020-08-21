76858fa390de0f9955c75f99f1d24c038471a88a
Grzegorz Nagórski <sparki_21@tlen.pl>
2020-08-21 10:14:55 +0200

    1

--- src/main/java/pl/javaStart/fxAction/controller/MainPainController.java
+++ src/main/java/pl/javaStart/fxAction/controller/MainPainController.java
@@ -1,20 +1,20 @@
 package pl.javaStart.fxAction.controller;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
 
 public class MainPainController {
 
     @FXML
-    private TextArea mainTextArea;
+    private TextArea LeftTextArea;
 
     @FXML
-    private Button lowerCaseButton;
+    private TextArea RightTextArea;
+
+    @FXML
+    private Button ButtonClerar;
+
 
     public void initialize() {
 //        mainTextArea.setOnKeyPressed(keyEvent -> System.out.println("Wciśnieto przycisk :" + keyEvent.getCharacter()));
@@ -32,17 +32,7 @@ public class MainPainController {
 //                mouseEvent -> System.out.println("mouse pressed"));
 //        mainTextArea.addEventFilter(MouseEvent.MOUSE_RELEASED,
 //                mouseEvent -> System.out.println("mouse released"));
-
-        mainTextArea.addEventFilter(MouseEvent.MOUSE_ENTERED,
-                mouseEvent -> System.out.println("Mysz w obszarze area" + " współrzędna wejścia X " +
-                        mouseEvent.getX() + " współrzędna wejścia Y " +
-                        mouseEvent.getY()));
-        mainTextArea.addEventFilter(MouseEvent.MOUSE_EXITED,
-                mouseEvent -> System.out.println("Mysz poza Area"));
-//        mainTextArea.addEventFilter(MouseEvent.MOUSE_MOVED,
-//                mouseEvent -> System.out.println("mysz porusza sie w Area"));
-        mainTextArea.addEventFilter(MouseEvent.MOUSE_DRAGGED,
-                mouseEvent -> System.out.println("Mysz porusza się w Area z wciśniętym przyciskiem"));
+        LeftTextArea.setText()
     }
 
 
